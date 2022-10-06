eadcc99df834361e10cdc26d35687e81c759c9c9
shixiansi <1765629830@qq.com>
2022-10-06 16:26:53 +0800

    提交

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
 /**
  * @Author: uixmsi
  * @Date: 2022-09-27 16:45:00
- * @LastEditTime: 2022-10-01 18:36:46
+ * @LastEditTime: 2022-10-06 16:12:52
  * @LastEditors: uixmsi
  * @Description: 
  * @FilePath: \Yunzai-Bot\plugins\qianyu-plugin\index.js
  * @版权声明
  **/
-import fs from "node:fs";
 import { init, startTask } from './lib/init.js'
 
 //打印启动日志
 logger.info(`--------------------------`);
 logger.info(`千羽初号机已启动~`);
 logger.info(`--------------------------`);
-
-
-//读取文件夹下所有的js文件
-const files = fs
-  .readdirSync("./plugins/qianyu-plugin/apps")
-  .filter((file) => file.endsWith(".js"));
 
 //加载定时任务
 await startTask()
@@ -28,6 +21,5 @@ await startTask()
 let main = new init()
 await main.load()
 let apps = main.apps
-console.log(apps)
 //导出
 export { apps };
