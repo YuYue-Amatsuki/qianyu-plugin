3808c7dfe32679316e8cda6d2a360814211c74a7
shixiansi <1765629830@qq.com>
2022-12-02 22:39:13 +0800

    修改引用撤回对其他消息反应

--- apps/component/chehui.js
+++ apps/component/chehui.js
@@ -14,15 +14,16 @@ apps.rule.push({
 
 async function yschehui(e) {
     if (e.source) {
-        if (e.member.is_admin || e.member.is_owner) {
-            this.reply("你是管理员，可以自己撤回消息哦！")
-        } else {
-            let reslut = await e.group.recallMsg(e.source.seq, e.source.rand)
-            if (!reslut) {
-                this.reply("伦家不是管理员，不能撤回超过两分钟的消息哦！请联系管理员撤回！")
+        if (e.source.user_id == e.self_id) {
+            if (e.member.is_admin || e.member.is_owner) {
+                this.reply("你是管理员，可以自己撤回消息哦！")
+            } else {
+                let reslut = await e.group.recallMsg(e.source.seq, e.source.rand)
+                if (!reslut) {
+                    this.reply("伦家不是管理员，不能撤回超过两分钟的消息哦！请联系管理员撤回！")
+                }
             }
         }
-
     }
 }
 
