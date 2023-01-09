090515f6c344c3365252c165f3414da46b590f94
shixiansi <1765629830@qq.com>
2023-01-09 19:02:07 +0800

    修复报时

--- apps/V3/Time/bs.js
+++ apps/V3/Time/bs.js
@@ -1,7 +1,8 @@
 import { segment } from 'oicq'
-import { ds, geturldata } from '../../../utils/index.js'
+import { ds, geturldata, filemage } from '../../../utils/index.js'
 import moment from 'moment'
 import lodash from 'lodash'
+let file = new filemage('plugins/miao-plugin/resources/')
 //语音报时的角色
 const ysjs = ["流浪者", "珐露珊", "莱依拉", "纳西妲", "妮露", "坎蒂丝", "赛诺", "多莉", "提纳里", "柯莱", "鹿野院平藏", "久岐忍", "夜兰", "空", "荧", "神里绫人", "八重神子", "云堇", "申鹤", "荒泷一斗", "五郎", "优菈", "阿贝多", "托马", "胡桃", "达达利亚", "雷电将军", "珊瑚宫心海", "埃洛伊", "宵宫", "神里绫华", "枫原万叶", "温迪", "刻晴", "莫娜", "可莉", "琴", "迪卢克", "七七", "魈", "钟离", "甘雨", "早柚", "九条裟罗", "凝光", "菲谢尔", "班尼特", "丽莎", "行秋", "迪奥娜", "安柏", "重云", "雷泽", "芭芭拉", "罗莎莉亚", "香菱", "凯亚", "北斗", "诺艾尔", "砂糖", "辛焱", "烟绯"]
 //报时的时间
@@ -28,8 +29,9 @@ function sleep(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+
+
 await ds("bs", `0 0 ${htime} * * *`, async () => {
-    console.log("出风啊");
     let grouplist = JSON.parse(await redis.get('qianyu:bstime:grouplist')) || []
     for (let g of grouplist) {
         let bscf = { ...bscfg, ...JSON.parse(await redis.get(`qianyu:bstime:config:${g}`)) }
@@ -48,9 +50,12 @@ await ds("bs", `0 0 ${htime} * * *`, async () => {
                 character = ysjs[lodash.random(0, ysjs.length - 1)]
             }
             await geturldata({ url: `http://www.whpioneer.xyz/api/getcharactercord/${ysjs.findIndex(item => item == character) + 1}`, data: ['data'], headers: { source: qySource } }, async res => {
+                let random = lodash.random(0, res.data.length - 1)
                 msg[0] = `${hour}点了，${res.data[random].text}`
                 if (bscf.isImg) {
-                    msg[1] = segment.image(`./plugins/qianyu-plugin/resources/img/可莉/可莉${lodash.random(1, 14)}.jpg`)
+                    character = character.replace('流浪者', '散兵')
+                    let list = file.getfilelist(`character-img/${character}/`).filter(item => item.includes(".jpg"))
+                    msg[1] = segment.image(`./plugins/miao-plugin/resources/character-img/${character}/${list[lodash.random(0, list.length - 1)]}`)
                 }
                 msg[2] = segment.record(res.data[random].url)
                 for (let m of msg) {
@@ -67,5 +72,24 @@ await ds("bs", `0 0 ${htime} * * *`, async () => {
 
     }
 })
+
+
+await cleardata()
+
+async function cleardata() {
+    let bslist = JSON.parse(await redis.get('qianyu:bstime:grouplist')) || []
+    let glist = Bot.gl
+    let grouplist = []
+    for (let g of glist) {
+        grouplist.push(g[0])
+    }
+    let newbslist = bslist.filter(item => grouplist.includes(item))
+    for (let b of bslist) {
+        if (!newbslist.includes(b)) {
+            await redis.del(`qianyu:bstime:config:${g}`)
+        }
+    }
+    await redis.set('qianyu:bstime:grouplist', JSON.stringify(newbslist))
+}
 
 export default apps
