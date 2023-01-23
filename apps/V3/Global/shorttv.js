d7b2ad2c036173d4ee839fae157822ef1edb8214
时先思 <1765629830@qq.com>
2023-01-23 05:08:23 +0000

    修改快手链接解析错误的问题
    Signed-off-by: 时先思 <1765629830@qq.com>

--- apps/V3/Global/shorttv.js
+++ apps/V3/Global/shorttv.js
@@ -36,7 +36,10 @@ export async function jx(e) {
 async function shorttvjx(e, msg) {
     //短链接解析
     const reg2 = /(https?|http|ftp|file):\/\/[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]/g;
-    let url = msg.match(reg2)[0].split("?")[0]
+    let url = msg.match(reg2)[0]
+    if(url.includes("?")){
+        url=url.split("?")[0]
+    }
     await api.getapi(`https://api.xcboke.cn/api/juhe?url=${url}`, ['data', 'url'], async (res) => {
         console.log("短视频解析中》》》》");
         let response = await fetch(res);
